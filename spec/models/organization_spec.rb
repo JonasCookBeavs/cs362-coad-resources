ea4877e6fe1f38b53fb23d67a4f0c5430f61183f
Kyle <canepak@oregonstate.edu>
2026-01-13 13:53:55 -0800

    updated organizations spec to check for attributes

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-  it "exists" do
-    Organization.new
-  end
+  subject(:organization) { Organization.new }
+
+  it { is_expected.to respond_to(:email) }
+  it { is_expected.to respond_to(:name) }
+  it { is_expected.to respond_to(:phone) }
+  it { is_expected.to respond_to(:status) }
+  it { is_expected.to respond_to(:primary_name) }
+  it { is_expected.to respond_to(:secondary_name) }
+  it { is_expected.to respond_to(:secondary_phone) }
+  it { is_expected.to respond_to(:description) }
+  it { is_expected.to respond_to(:rejection_reason) }
+  it { is_expected.to respond_to(:liability_insurance) }
+  it { is_expected.to respond_to(:title) }
+  it { is_expected.to respond_to(:transportation) }
+  it { is_expected.to respond_to(:approve) }
+  it { is_expected.to respond_to(:reject) }
+  it { is_expected.to respond_to(:to_s) }
+  it { is_expected.to respond_to(:set_default_status) }
+
 end
