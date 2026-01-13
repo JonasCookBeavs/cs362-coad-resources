5e5ef50f82281a818e63b98f819821167f945c3b
Kyle <canepak@oregonstate.edu>
2026-01-13 14:06:04 -0800

    fixed organization spec formatting

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,23 +1,85 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-  subject(:organization) { Organization.new }
+  
+  it "has an email" do
+    organization = Organization.new
+    expect(organization).to respond_to(:email)
+  end
 
-  it { is_expected.to respond_to(:email) }
-  it { is_expected.to respond_to(:name) }
-  it { is_expected.to respond_to(:phone) }
-  it { is_expected.to respond_to(:status) }
-  it { is_expected.to respond_to(:primary_name) }
-  it { is_expected.to respond_to(:secondary_name) }
-  it { is_expected.to respond_to(:secondary_phone) }
-  it { is_expected.to respond_to(:description) }
-  it { is_expected.to respond_to(:rejection_reason) }
-  it { is_expected.to respond_to(:liability_insurance) }
-  it { is_expected.to respond_to(:title) }
-  it { is_expected.to respond_to(:transportation) }
-  it { is_expected.to respond_to(:approve) }
-  it { is_expected.to respond_to(:reject) }
-  it { is_expected.to respond_to(:to_s) }
-  it { is_expected.to respond_to(:set_default_status) }
+  it "has a name" do
+    organization = Organization.new
+    expect(organization).to respond_to(:name)
+  end
+
+  it "has a phone" do
+    organization = Organization.new
+    expect(organization).to respond_to(:phone)
+  end
+
+  it "has a status" do
+    organization = Organization.new
+    expect(organization).to respond_to(:status)
+  end
+
+  it "has a primary name" do
+    organization = Organization.new
+    expect(organization).to respond_to(:primary_name)
+  end
+
+  it "has a secondary name" do
+    organization = Organization.new
+    expect(organization).to respond_to(:secondary_name)
+  end
+
+  it "has a secondary phone" do
+    organization = Organization.new
+    expect(organization).to respond_to(:secondary_phone)
+  end
+
+  it "has a description" do
+    organization = Organization.new
+    expect(organization).to respond_to(:description)
+  end
+
+  it "has a rejection reason" do
+    organization = Organization.new
+    expect(organization).to respond_to(:rejection_reason)
+  end
+
+  it "has liability insurance" do
+    organization = Organization.new
+    expect(organization).to respond_to(:liability_insurance)
+  end
+
+  it "has a title" do
+    organization = Organization.new
+    expect(organization).to respond_to(:title)
+  end
+
+  it "has transportation" do
+    organization = Organization.new
+    expect(organization).to respond_to(:transportation)
+  end
+
+  it "has approve" do
+    organization = Organization.new
+    expect(organization).to respond_to(:approve)
+  end
+
+  it "has reject" do
+    organization = Organization.new
+    expect(organization).to respond_to(:reject)
+  end
+
+  it "has to_s" do
+    organization = Organization.new
+    expect(organization).to respond_to(:to_s)
+  end
+
+  it "has set_default_status" do
+    organization = Organization.new
+    expect(organization).to respond_to(:set_default_status)
+  end
 
 end
