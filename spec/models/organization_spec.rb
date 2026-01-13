d467262f5161eab6b6e9db28e2039a2831e66863
Kyle <canepak@oregonstate.edu>
2026-01-13 12:59:17 -0800

    fixed spec formats

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
   it "exists" do
-        Organization.new
-    end
+    Organization.new
+  end
 end
--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
   it "exists" do
-        ResourceCategory.new
-    end
+    ResourceCategory.new
+  end
 end
