2f8078e2e5c0384d7a22b17afe966b9596cd9605
JonasCookBeavs <jonascook04@gmail.com>
2026-02-18 13:16:34 -0800

    Resource Categories Spec complete

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -1,5 +1,111 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
+  
+  describe 'as a logged-out user' do
+    let (:user) { create(:user) }
+    let (:resource_category) { create(:resource_category) }
 
+    it {
+      expect(get(:index)).to redirect_to new_user_session_path
+    }
+
+    it {
+      post(:create, params: { resource_category: FactoryBot.attributes_for(:resource_category) })
+      expect(response).to redirect_to new_user_session_path
+    }
+
+    it {
+      expect(get(:show, params: { id: resource_category.id })).to redirect_to new_user_session_path
+    }
+
+    it {
+      expect(get(:new)).to redirect_to new_user_session_path
+    }
+
+    it {
+      expect(get(:edit, params: { id: resource_category.id })).to redirect_to new_user_session_path
+    }
+
+    it {
+      expect(patch(:update, params: { id: resource_category.id })).to redirect_to new_user_session_path
+    }
+
+    it {
+      expect(delete(:show, params: { id: resource_category.id })).to redirect_to new_user_session_path
+    }
+
+  end
+
+  describe 'as a logged-in user' do
+    let (:user) { create(:user) }
+    let (:resource_category) { create(:resource_category) }
+
+    before (:each) { sign_in user }
+
+    it {
+      expect(get(:index)).to redirect_to dashboard_path
+    }
+
+    it {
+      post(:create, params: { resource_category: FactoryBot.attributes_for(:resource_category) })
+      expect(response).to redirect_to dashboard_path
+    }
+
+    it {
+      expect(get(:show, params: { id: resource_category.id })).to redirect_to dashboard_path
+    }
+
+    it {
+      expect(get(:new)).to redirect_to dashboard_path
+    }
+
+    it {
+      expect(get(:edit, params: { id: resource_category.id })).to redirect_to dashboard_path
+    }
+
+    it {
+      expect(patch(:update, params: { id: resource_category.id })).to redirect_to dashboard_path
+    }
+
+    it {
+      expect(delete(:show, params: { id: resource_category.id })).to redirect_to dashboard_path
+    }
+  end
+
+  describe 'as an admin' do
+    let (:user) { create(:user, :admin) }
+    let (:resource_category) { create(:resource_category) }
+
+    before (:each) { sign_in user }
+
+    it {
+      expect(get(:index)).to be_successful
+    }
+
+    it {
+      post(:create, params: { id: resource_category.id, resource_category: FactoryBot.attributes_for(:resource_category) })
+      expect(response).to be_successful
+    }
+
+    it {
+      expect(get(:show, params: { id: resource_category.id })).to be_successful
+    }
+
+    it {
+      expect(get(:new)).to be_successful
+    }
+
+    it {
+      expect(get(:edit, params: { id: resource_category.id })).to be_successful
+    }
+
+    it {
+      expect(patch(:update, params: { id: resource_category.id, resource_category: FactoryBot.attributes_for(:resource_category) })).to redirect_to @resource_category
+    }
+
+    it {
+      expect(delete(:show, params: { id: resource_category.id })).to be_successful
+    }
+  end
 end
