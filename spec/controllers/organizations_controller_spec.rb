cc2786074d5ec56214cbeb7fd303e126883a9763
JonasCookBeavs <jonascook04@gmail.com>
2026-02-18 12:53:01 -0800

    Organizations Controller Spec complete

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -1,5 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe OrganizationsController, type: :controller do
+  describe 'as a logged-out user' do
+    let (:user) { create(:user) }
+    let (:organization) { create(:organization) }
 
+    it {
+      expect(get(:index)).to redirect_to new_user_session_path
+    }
+
+    it {
+      expect(get(:new)).to redirect_to new_user_session_path
+    }
+
+    it {
+      post(:create, params: { organization: FactoryBot.attributes_for(:organization) })
+      expect(response).to redirect_to new_user_session_path
+    }
+
+    it {
+      expect(patch(:update, params: { id: organization.id })).to redirect_to new_user_session_path
+    }
+
+  end
+
+  describe 'as a logged-in user' do
+    let (:user) { create(:user) }
+    let (:organization) { create(:organization) }
+
+    before (:each) { sign_in user }
+
+    it {
+      expect(get(:index)).to be_successful
+    }
+
+    it {
+      expect(get(:new)).to be_successful
+    }
+  
+    # Skip for now
+    # it {
+    #   post(:create, params: { organization: FactoryBot.attributes_for(:organization) })
+    #   expect(response).to redirect_to dashboard_path
+    # }
+
+    it {
+      expect(patch(:update, params: { id: organization.id })).to redirect_to dashboard_path
+    }
+
+  end
+
+  describe 'as an admin' do
+    let (:user) { create(:user, :admin) }
+    let (:organization) { create(:organization) }
+
+    before (:each) { sign_in user }
+
+    it {
+      expect(get(:index)).to be_successful
+    }
+
+    it {
+      expect(get(:new)).to redirect_to dashboard_path
+    }
+
+    it {
+      post(:create, params: { organization: FactoryBot.attributes_for(:organization) })
+      expect(response).to redirect_to dashboard_path
+    }
+
+    it {
+      expect(patch(:update, params: { id: organization.id, organization: FactoryBot.attributes_for(:organization) })).to redirect_to dashboard_path
+    }
+
+  end
 end
