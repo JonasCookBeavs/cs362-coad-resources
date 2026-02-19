4471b2163d3835bc9235151c4d0121b0cf755451
Kyle <canepak@oregonstate.edu>
2026-02-18 16:16:59 -0800

    finished goal 7 for controller specs that are currently filled out and commented some out until I can get feedback on them

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -31,6 +31,22 @@ RSpec.describe RegionsController, type: :controller do
       expect(patch(:update, params: { id: region.id })).to redirect_to new_user_session_path
     }
 
+    it {
+      region = build(:region)
+      allow(region).to receive(:save).and_return(false)
+      allow(Region).to receive(:new).and_return(region)
+      post(:create, params: { region: FactoryBot.attributes_for(:region) })
+      expect(response).to redirect_to new_user_session_path
+    }
+
+    it {
+      region = create(:region)
+      allow(region).to receive(:update).and_return(false)
+      allow(Region).to receive(:find).and_return(region)
+      post(:update, params: { id: region.id })
+      expect(response).to redirect_to new_user_session_path
+    }
+
   end
 
   describe 'as a logged-in user' do
@@ -64,6 +80,22 @@ RSpec.describe RegionsController, type: :controller do
       expect(patch(:update, params: { id: region.id })).to redirect_to dashboard_path
     }
 
+    it {
+      region = build(:region)
+      allow(region).to receive(:save).and_return(false)
+      allow(Region).to receive(:new).and_return(region)
+      post(:create, params: { region: FactoryBot.attributes_for(:region) })
+      expect(response).to redirect_to dashboard_path
+    }
+
+    it {
+      region = create(:region)
+      allow(region).to receive(:update).and_return(false)
+      allow(Region).to receive(:find).and_return(region)
+      post(:update, params: { id: region.id })
+      expect(response).to redirect_to dashboard_path
+    }
+
   end
 
   describe 'as an admin' do
@@ -95,6 +127,22 @@ RSpec.describe RegionsController, type: :controller do
 
     it {
       expect(patch(:update, params: { id: region.id, region: FactoryBot.attributes_for(:region) })).to redirect_to @region
+    }
+    
+    it {
+      region = build(:region)
+      allow(region).to receive(:save).and_return(false)
+      allow(Region).to receive(:new).and_return(region)
+      post(:create, params: { region: FactoryBot.attributes_for(:region) })
+      expect(response).to be_successful
+    }
+
+    it {
+      region = create(:region)
+      allow(region).to receive(:update).and_return(false)
+      allow(Region).to receive(:find).and_return(region)
+      post(:update, params: { id: region.id, region: FactoryBot.attributes_for(:region) })
+      expect(response).to be_successful
     }
 
   end
--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -35,6 +35,38 @@ RSpec.describe ResourceCategoriesController, type: :controller do
       expect(delete(:show, params: { id: resource_category.id })).to redirect_to new_user_session_path
     }
 
+    it {
+      res_cat = build(:resource_category)
+      allow(res_cat).to receive(:save).and_return(false)
+      allow(ResourceCategory).to receive(:new).and_return(res_cat)
+      post(:create, params: { resource_category: FactoryBot.attributes_for(:resource_category) })
+      expect(response).to redirect_to new_user_session_path
+    }
+
+    it {
+      res_cat = create(:resource_category)
+      allow(res_cat).to receive(:update).and_return(false)
+      allow(ResourceCategory).to receive(:find).and_return(res_cat)
+      patch :update, params: { id: res_cat.id, resource_category: FactoryBot.attributes_for(:resource_category) }
+      expect(response).to have_http_status(:found)
+    }
+
+    it {
+      res_cat = create(:resource_category)
+      allow(res_cat).to receive(:activate).and_return(false)
+      allow(ResourceCategory).to receive(:find).and_return(res_cat)
+      patch :activate, params: { id: res_cat.id, resource_category: FactoryBot.attributes_for(:resource_category) }
+      expect(response).to redirect_to new_user_session_path
+    }
+
+    it {
+      res_cat = create(:resource_category)
+      allow(res_cat).to receive(:deactivate).and_return(false)
+      allow(ResourceCategory).to receive(:find).and_return(res_cat)
+      patch :deactivate, params: { id: res_cat.id, resource_category: FactoryBot.attributes_for(:resource_category) }
+      expect(response).to redirect_to new_user_session_path
+    }
+
   end
 
   describe 'as a logged-in user' do
@@ -71,6 +103,39 @@ RSpec.describe ResourceCategoriesController, type: :controller do
     it {
       expect(delete(:show, params: { id: resource_category.id })).to redirect_to dashboard_path
     }
+
+    it {
+      res_cat = build(:resource_category)
+      allow(res_cat).to receive(:save).and_return(false)
+      allow(ResourceCategory).to receive(:new).and_return(res_cat)
+      post(:create, params: { resource_category: FactoryBot.attributes_for(:resource_category) })
+      expect(response).to redirect_to dashboard_path
+    }
+
+    it {
+      res_cat = create(:resource_category)
+      allow(res_cat).to receive(:update).and_return(false)
+      allow(ResourceCategory).to receive(:find).and_return(res_cat)
+      patch :update, params: { id: res_cat.id, resource_category: FactoryBot.attributes_for(:resource_category) }
+      expect(response).to have_http_status(:found)
+    }
+
+    it {
+      res_cat = create(:resource_category)
+      allow(res_cat).to receive(:activate).and_return(false)
+      allow(ResourceCategory).to receive(:find).and_return(res_cat)
+      patch :activate, params: { id: res_cat.id, resource_category: FactoryBot.attributes_for(:resource_category) }
+      expect(response).to redirect_to dashboard_path
+    }
+
+    it {
+      res_cat = create(:resource_category)
+      allow(res_cat).to receive(:deactivate).and_return(false)
+      allow(ResourceCategory).to receive(:find).and_return(res_cat)
+      patch :deactivate, params: { id: res_cat.id, resource_category: FactoryBot.attributes_for(:resource_category) }
+      expect(response).to redirect_to dashboard_path
+    }
+
   end
 
   describe 'as an admin' do
@@ -107,5 +172,38 @@ RSpec.describe ResourceCategoriesController, type: :controller do
     it {
       expect(delete(:show, params: { id: resource_category.id })).to be_successful
     }
+
+    it {
+      res_cat = build(:resource_category)
+      allow(res_cat).to receive(:save).and_return(false)
+      allow(ResourceCategory).to receive(:new).and_return(res_cat)
+      post(:create, params: { resource_category: FactoryBot.attributes_for(:resource_category) })
+      expect(response).to be_successful
+    }
+
+    it {
+      res_cat = create(:resource_category)
+      allow(res_cat).to receive(:update).and_return(false)
+      allow(ResourceCategory).to receive(:find).and_return(res_cat)
+      patch :update, params: { id: res_cat.id, resource_category: FactoryBot.attributes_for(:resource_category) }
+      expect(response).to have_http_status(:ok)
+    }
+
+    it {
+      res_cat = create(:resource_category)
+      allow(res_cat).to receive(:activate).and_return(false)
+      allow(ResourceCategory).to receive(:find).and_return(res_cat)
+      patch :activate, params: { id: res_cat.id, resource_category: FactoryBot.attributes_for(:resource_category) }
+      expect(response).to redirect_to resource_category_path
+    }
+
+    it {
+      res_cat = create(:resource_category)
+      allow(res_cat).to receive(:deactivate).and_return(false)
+      allow(ResourceCategory).to receive(:find).and_return(res_cat)
+      patch :deactivate, params: { id: res_cat.id, resource_category: FactoryBot.attributes_for(:resource_category) }
+      expect(response).to redirect_to resource_category_path
+    }
+
   end
 end
