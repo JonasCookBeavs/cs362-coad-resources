a8ae4df751aa26336c7e7698c542af86edebc539
Kyle <canepak@oregonstate.edu>
2026-02-25 13:32:00 -0800

    added tests not from root path for create ticket

--- spec/features/tickets/create_ticket_spec.rb
+++ spec/features/tickets/create_ticket_spec.rb
@@ -31,5 +31,26 @@ RSpec.describe 'Creating a Ticket', type: :feature do
     # ... [expectations] ...
     expect(current_path).to eq current_path
   end
+
+  it 'can be done by going directly to the form' do
+    visit new_ticket_path
+    fill_in 'Full Name', with: 'Test Name'
+    fill_in 'Phone Number', with: '555-555-1234'
+    select @region.name, from: 'Region'
+    select @resource_category.name, from: 'Resource Category'
+    fill_in 'Description', with: 'Test description'
+    click_on 'Send this help request'
+
+    # ... [expectations] ...
+    expect(current_path).to eq ticket_submitted_path
+  end
+
+  it 'can fail when going directly to the form' do
+    visit new_ticket_path
+    click_on 'Send this help request'
+
+    # ... [expectations] ...
+    expect(current_path).to eq current_path
+  end
   
 end
