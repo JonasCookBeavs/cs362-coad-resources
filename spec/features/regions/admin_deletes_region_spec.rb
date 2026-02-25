32c8d1fb6d4f22ebfba85fdca92afdb9765ab33b
JonasCookBeavs <jonascook04@gmail.com>
2026-02-25 13:31:28 -0800

    Admin deletes region

--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Region', type: :feature do
+  let (:admin) { create(:user, :admin) }
+  
+  it "deletes a region" do
+    region = create(:region, name: "FAKE") # or Region.create!(name: "FAKE")
+    log_in_as(admin)
 
+    visit regions_path
+    expect(page).to have_text("FAKE")
+
+    # Option A: the button/link is literally labeled "Delete"
+    accept_confirm do
+      click_on "Delete", match: :first
+    end
+
+    expect(page).not_to have_text("FAKE")
+    expect(Region.exists?(region.id)).to be(false)
+  end
 end
